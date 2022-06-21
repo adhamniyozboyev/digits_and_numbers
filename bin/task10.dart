10e05a4de5a2983fb0dbe5d2774c621d83f75ccd
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-06-21 11:44:27 +0500

    task

--- bin/task10.dart
+++ bin/task10.dart
@@ -8,4 +8,14 @@
   Returns:
       return answer
   */
-void main() {}
+
+
+  int func (number){
+    int r=number~/1000;
+    int w=number%1000~/100;
+    int q=number%1000%100~/10;
+    int e=number%1000%100%10;
+    return r*w*q*e;
+  }
+void main() {print(func(2346));
+}
